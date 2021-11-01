ed6756b6216fdc8c6605d1cc49ab0d876fbf6ecc
Yan-Coquoz <yan.coquoz@gmail.com>
2021-11-01 20:20:12 +0100

    Ajout de map et de css

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,32 @@ import React, { FunctionComponent, useState, useEffect } from "react";
 import POKEMON from "./Models/mock-pokemon"; // import des données
 import Pokemon from "./Models/pokemon"; // import des types
 
+import "./style.css";
+
 const App: FunctionComponent = () => {
   // const [name, setName] = useState<String>("Yan");
-  const [pokemons, setPokemon] = useState<Pokemon[] | []>([]);
+  const [pokemons, setPokemon] = useState<Pokemon[]>([]);
   useEffect(() => {
     setPokemon(POKEMON);
   }, []);
 
   return (
-    <div>
+    <>
       <h1>Pokémons</h1>
       <p>il y a {pokemons.length} pokemons dans mon tableau !</p>
-    </div>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id} className="Card">
+            <img src={pokemon.picture} alt={pokemon.name} />
+            <div>
+              <p>{pokemon.name}</p> <p>{pokemon.hp} HP</p>
+              <p>{pokemon.cp} CP</p>
+              <p>{pokemon.types.map((type) => type + " ")}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
