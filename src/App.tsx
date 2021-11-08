3aeaffcf1d021d173c5938bb56b7677470e0c50c
Yan-Coquoz <yan.coquoz@gmail.com>
2021-11-08 19:09:55 +0100

    Mise en place de routes et le composant PokemonDetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 import React, { FunctionComponent } from "react";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import PokemonList from "./pages/PokemonList";
+import PokemonDetail from "./pages/PokemonDetail";
 import "./style.css";
 
 const App: FunctionComponent = () => {
   return (
-    <div>
-      <PokemonList />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <div className="nav-wrapper teal">
+            <Link to="/" className="brand-logo center">
+              Pokédex
+            </Link>
+          </div>
+        </nav>
+        <Switch>
+          <Route exact path="/" component={PokemonList} />
+          <Route exact path="/pokemons" component={PokemonList} />
+          <Route exact path="/pokemons/:id" component={PokemonDetail} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
